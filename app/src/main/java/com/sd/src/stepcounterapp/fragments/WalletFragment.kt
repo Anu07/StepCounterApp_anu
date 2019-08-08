938d99c7a427df9f67d94029bbddf41451654658
shubhamv <shubham.vema740@gmail.com>
2019-08-08 14:35:13 +0530

    shubham Changes

--- app/src/main/java/com/sd/src/stepcounterapp/fragments/WalletFragment.kt
+++ app/src/main/java/com/sd/src/stepcounterapp/fragments/WalletFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -52,8 +51,7 @@ class WalletFragment : Fragment() {
             Observer<TokenModel> { mData ->
                 if (mData.data != null) {
                     txtTokens.text = mData.data!!.totalEarntokens.toString()
-                } else {
-                    Toast.makeText(mContext, mData.message, Toast.LENGTH_SHORT).show()
+                    txtSteps.text = mData.data!!.totalSteps.toString()
                 }
             })
 
